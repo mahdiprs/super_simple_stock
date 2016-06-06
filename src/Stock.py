b12c2a5576f372fc535615a6a61757ce75ada9d7
mahdi <mahdiprs@gmail.com>
2016-06-06 11:31:45 +0100

    Created a class to store the stock table function file has been added
    The structure has been added
    
    Break to several module
    
    update files header
    
    fixed the last price

--- src/Stock.py
+++ src/Stock.py
@@ -6,7 +6,6 @@ from Trade import Trade
 
 # create class stock
 class Stock(object):
-    all_trades = list()  # a list to keep track of all trades
     FIFTEEN_MINUTES = 15 * 60
 
     def __init__(self, stock_symbol, stock_type, last_dividend, fixed_dividend, par_value, stock_price=0):
@@ -18,6 +17,7 @@ class Stock(object):
         self.fixed_dividend = fixed_dividend
         self.par_value = par_value
         self.stock_price = stock_price
+        self.all_trades = list()  # a list to keep track of all trades
 
     (" a method for validating the stock symbol.\n"
      "    Symbols are {\"TEA\", \"POP\", \"ALE\", \"GIN\", \"JOE\"} ")
@@ -65,20 +65,18 @@ class Stock(object):
             return "The P/E ratio is not defined for stock %s since dividend yield is zero. " % self.stock_symbol
 
     def record_trade(self, quantity_of_share, trade_indicator, trade_price):
-        # type: (int, str, float) -> Stock
+        # type: (int, str, float) -> Trade
 
         """ Record a trade for a given stock with quantity of share, trade indicator, trade price.
         the trade indicator is either buy or sell """
 
         trade_time = time.time()  # current time
-        trade = Trade(self.stock_symbol, trade_time, quantity_of_share, trade_indicator, trade_price)
-        if trade:
+        try:
+            trade = Trade(self.stock_symbol, trade_time, quantity_of_share, trade_indicator, trade_price)
             self.all_trades.append(trade)
             print ("Adding a new trade was successful.")
-            return True
-        else:
-            print ("Adding a new trade was not successful.")
-            return False
+        except:
+            raise TypeError("Adding a new trade was not successful.")
 
     @property
     def calculate_volume_stock_price(self):
@@ -101,10 +99,8 @@ class Stock(object):
     @property
     def stock_last_trade_price(self):
         """ Find the latest trade recorded and the latest price for a given stock"""
-        latest_trade_time = 0
-        latest_trade_price = 0
-        for a_trade in self.all_trades:
-            if a_trade.trade_time >= latest_trade_time:
-                latest_trade_time = a_trade.trade_time
-                latest_trade_price = a_trade.trade_price
+
+        num_recorded_trades = len(self.all_trades)
+        latest_trade = self.all_trades[num_recorded_trades-1]
+        latest_trade_price = latest_trade.trade_price
         return latest_trade_price
--- test/test.py
+++ test/test.py
@@ -5,25 +5,35 @@ import unittest
 
 class SimpleStockMarketTest(unittest.TestCase):
     """ Test Simple stock functions."""
-    stock_TEA = Stock("TEA", "Common", 8, 7, 100)
-    stock_GIN = Stock("GIN", "Common", 2, 3, 12)
+    stock_TEA = Stock("TEA", "Common", 0, 0, 100)
+    stock_POP = Stock("POP", "Common", 8, 0, 100)
+    stock_ALE = Stock("ALE", "Common", 23, 0, 60)
+    stock_GIN = Stock("GIN", "Preferred", 8, 2, 100)
+    stock_JOE = Stock("JOE", "Common", 13, 0, 250)
 
     def test_record_trade(self):
         """ Does it record a trade successfully. """
 
-        self.assertTrue(self.stock_TEA.record_trade(2, "sell", 8))
-        self.assertTrue(self.stock_TEA.record_trade(5, "buy", 4))
+        self.assertIsNone(self.stock_TEA.record_trade(2, "sell", 1))
+        self.assertIsNone(self.stock_TEA.record_trade(5, "buy", 2))
+        self.assertIsNone(self.stock_POP.record_trade(5, "buy", 4))
+        self.assertIsNone(self.stock_POP.record_trade(5, "Bell", 4))
 
     """ record two trades for testing the list of all trades"""
-    stock_TEA.record_trade(2, "sell", 8)
+    stock_TEA.record_trade(2, "sell", 3)
     stock_TEA.record_trade(5, "buy", 4)
     stock_GIN.record_trade(7, "sell", 7)
+    stock_GIN.record_trade(7, "sell", 8)
 
     stocks = {stock_TEA, stock_GIN}
 
+    for item in stock_TEA.all_trades:
+        print (item.trade_price)
+
+
     def test_get_all_trades(self):
         """ Check all trades recorded """
-        self.assertEqual(len(self.stock_TEA.all_trades), 3)
+        self.assertEqual(len(self.stock_TEA.all_trades), 7)
 
     def test_dividend_yield(self):
         """ Test answer  dividend yield calculation is correct."""
